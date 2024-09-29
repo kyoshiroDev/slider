b806461ad54eb7d582fc4e63fd44105fcfff170d
greg <tahir.gregory.dev@gmail.com>
2024-09-29 14:42:12 +0200

    start projet

---
+++ src/data/sliderData.js
@@ -0,0 +1,23 @@
+ const sliderData = [
+  {
+    id: 1,
+    description: "Leaving room"
+  },
+  {
+    id: 2,
+    description: "Kitchen"
+  },
+  {
+    id: 3,
+    description: "Bedroom"
+  },
+  {
+    id: 4,
+    description: "Bathroom"
+  },
+  {
+    id: 5,
+    description: "Balcony"
+  },
+]
+export default sliderData;
